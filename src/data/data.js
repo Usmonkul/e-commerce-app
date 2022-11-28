3f04933589388a2b755ce2c598b00f6d49cdd88d
Usmon <usmonhakimov1999@gmail.com>
2022-11-28 19:02:28 +0900

    add small handmade dataset

---
+++ src/data/data.js
@@ -0,0 +1,153 @@
+export const data = {
+  email: "usmonhakimov1999@gmail.com",
+  laptop: [
+    {
+      name: "Microsoft Surface 3",
+      price: "700",
+      ram: "16",
+      image:
+        "https://images.unsplash.com/photo-1595245150809-ef9436aaded6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      memory: "256",
+    },
+    {
+      name: "MacBook Pro M1",
+      price: "1100",
+      ram: "8",
+      image:
+        "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+      memory: "256",
+    },
+    {
+      name: "MacBook Air M2",
+      price: "1000",
+      ram: "8",
+      image:
+        "https://images.unsplash.com/photo-1606248897020-696650b29ee1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+      memory: "512",
+    },
+    {
+      name: "MacBook Pro M2",
+      price: "2500",
+      ram: "16",
+      image:
+        "https://images.unsplash.com/photo-1541807084-5c52b6b3adef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      memory: "512",
+    },
+    {
+      name: "Asus ZenBook",
+      price: "1300",
+      ram: "16",
+      image:
+        "https://images.unsplash.com/photo-1630794180018-433d915c34ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
+      memory: "1112",
+    },
+    {
+      name: "Asus ",
+      price: "500",
+      ram: "8",
+      image:
+        "https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
+      memory: "256",
+    },
+    {
+      name: "Acer",
+      price: "200",
+      ram: "8",
+      image:
+        "https://images.unsplash.com/photo-1525972183131-2f5c48254ec4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80",
+      memory: "128",
+    },
+    {
+      name: "Microsoft Surface 2",
+      price: "500",
+      ram: "8",
+      image:
+        "https://images.unsplash.com/photo-1661961110372-8a7682543120?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      memory: "256",
+    },
+  ],
+  phone: [
+    {
+      name: "iPhone 14",
+      memory: "64",
+      price: "1400",
+      image:
+        "https://images.unsplash.com/photo-1664478546384-d57ffe74a78c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      name: "iPhone 13 Pro",
+      memory: "64",
+      price: "1400",
+      image:
+        "https://images.unsplash.com/photo-1663761879666-312880c27fc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+    },
+    {
+      name: "Galaxy Z Flip",
+      memory: "64",
+      price: "1400",
+      image:
+        "https://images.unsplash.com/photo-1592813491527-b34ff452c600?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      name: "Samsung S21 Ultra",
+      memory: "64",
+      price: "1400",
+      image:
+        "https://images.unsplash.com/photo-1610945415295-d9bbf067e59c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
+    },
+    {
+      name: "Galaxy Note 20 ",
+      memory: "64",
+      price: "600",
+      image:
+        "https://images.unsplash.com/photo-1608773098426-174c5285433c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=762&q=80",
+    },
+    {
+      name: "Galaxy Fold",
+      memory: "512",
+      price: "900",
+      image:
+        "https://images.unsplash.com/photo-1661523720020-44faea7163d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    },
+    {
+      name: "iPhone 14 Pro Max",
+      memory: "512",
+      price: "1700",
+      image:
+        "https://images.unsplash.com/photo-1606341802409-aceb7ebbec1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+    },
+    {
+      name: "iPhone 14 Mini",
+      memory: "512",
+      price: "500",
+      image:
+        "https://images.unsplash.com/photo-1632505084030-9b97405cb28d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    },
+  ],
+  watch: [
+    {
+      name: "Smart Galaxy Watch 3",
+      price: "199.00",
+      image:
+        "https://images.unsplash.com/photo-1557438159-51eec7a6c9e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      name: "Apple Watch 3",
+      price: "499.00",
+      image:
+        "https://images.unsplash.com/photo-1624096104992-9b4fa3a279dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=702&q=80",
+    },
+    {
+      name: "Xiomi Watch 2",
+      price: "99.00",
+      image:
+        "https://images.unsplash.com/photo-1551816230-ef5deaed4a26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
+    },
+    {
+      name: "Apple Watch 7",
+      price: "290.00",
+      image:
+        "https://images.unsplash.com/photo-1640943676462-a396ebdbb5d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    },
+  ],
+};
